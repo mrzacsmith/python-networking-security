d5e215cfe7a9931b97293f70f0ce16669844ac68
Zac Smith <zrs3141592@gmail.com>
2019-01-24 21:01:50 -0700

    work with conditionals

---
+++ Conditionals.py
@@ -0,0 +1,11 @@
+__author__ = 'silverback'
+
+x  = int(input("Provide me with a number: "))
+
+if (x < 1):
+    print("Too little")
+elif (x > 1 and x < 10):
+    print("Just about right")
+else:
+    print("Too high")
+
