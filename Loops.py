53892c1428151f43db436741245f24c7b756cd5b
Zac Smith <zrs3141592@gmail.com>
2019-01-24 20:31:07 -0700

    add loops

---
+++ Loops.py
@@ -0,0 +1,18 @@
+name = input("What is your name: ")
+
+for i in range(1,10):
+    print(name, i)
+
+x = 0
+while True:
+    print(x)
+    x += 1
+    if (x == 15):
+        break
+
+
+mylist = ['lions', 'tigers', 'bears']
+
+for i in mylist:
+    print(i)
+
