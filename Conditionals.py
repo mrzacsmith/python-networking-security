570f06c34a3a05f7d8b6eed4a761ecde75095d31
Zac Smith <zrs3141592@gmail.com>
2019-01-24 21:04:20 -0700

    correct if statement contradiction

--- Conditionals.py
+++ Conditionals.py
@@ -4,7 +4,7 @@ x  = int(input("Provide me with a number: "))
 
 if (x < 1):
     print("Too little")
-elif (x > 1 and x < 10):
+elif (x >= 1 and x =< 10):
     print("Just about right")
 else:
     print("Too high")
